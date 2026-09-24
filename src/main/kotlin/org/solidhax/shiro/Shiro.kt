fe773cb756427343d36723178b133b9515a8a670
SoLIDHAX404 <toastbrotgaming@gmx.at>
2026-09-24 13:11:59 +0200

    revert(MotionBlur): removed the motion blur module
    Reverts d37a1b8, 9c25ff6 and 0330aea, keeping the AspectRatio comment cleanup that landed in 9c25ff6.

--- src/main/kotlin/org/solidhax/shiro/Shiro.kt
+++ src/main/kotlin/org/solidhax/shiro/Shiro.kt
@@ -18,7 +18,6 @@ import org.solidhax.shiro.features.ModuleManager
 import org.solidhax.shiro.features.impl.mining.CorpseESP
 import org.solidhax.shiro.features.impl.misc.ArrayListModule
 import org.solidhax.shiro.features.impl.misc.AspectRatio
-import org.solidhax.shiro.features.impl.misc.MotionBlur
 import org.solidhax.shiro.features.impl.misc.TestModule
 import org.solidhax.shiro.utils.render.RenderBatchManager
 import org.solidhax.shiro.utils.skyblock.LocationUtils
@@ -44,7 +43,6 @@ object Shiro : ClientModInitializer {
             CorpseESP,
             ArrayListModule,
             AspectRatio,
-            MotionBlur,
         )
 
         HudElementRegistry.attachElementBefore(VanillaHudElements.CHAT, Identifier.fromNamespaceAndPath(MOD_ID, "hud"), ModuleManager::renderHud)
